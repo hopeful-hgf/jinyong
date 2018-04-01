8f52e92b2f0b9561afc73ca6041395c933b3bf52
jinlong <jinlong@divemath.com>
2018-04-01 11:21:06 +0800

    add google search

--- common/__init__.py
+++ common/__init__.py
@@ -1,6 +1,9 @@
 #coding:utf-8
 
-from .common import _try
+from .common import _try, parse, save
+from .crawler import crawler, parser
+from .pipeline import multi
 from .model import Jinyong as jy
+from .dumblog import dlog
 
-__all__ = ['_try', 'jy']
+__all__ = ['crawler', 'parse', 'save', '_try', 'jy', 'dlog', 'multi', 'parser']
---
+++ common/crawler.py
@@ -0,0 +1,42 @@
+# !/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import requests
+import time
+import yaml
+from lxml import etree
+
+with open('settings.yaml', 'r') as file:
+    config = yaml.load(file)
+
+
+def crawler(url):
+    headers = {
+        'Cookie':
+        config['cookie'],
+        'User-Agent':
+        'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36'
+    }
+    print('crawling : %s' % (url))
+    try:
+        response = requests.get(url, headers=headers)
+        print('status_code is %s' % response.status_code)
+        time.sleep(config['time_sleep'])
+        # return response.content
+        result = etree.HTML(response.content)
+        return result
+    except Exception as err:
+        print(err)
+        return None
+
+
+def parser(lis, word=''):
+    if len(lis) == 0:
+        return ''
+    elif len(lis) == 1:
+        if lis[0] == '':
+            return ''
+        else:
+            return lis[0].strip()
+    elif len(lis) > 1:
+        return word.join(lis).replace('  ', '').replace('\r', '').replace('\n', '').strip()
